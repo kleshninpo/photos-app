5f902a24e3dfc1b33dffb23ca4de7cc9dba77b50
kleshninpo <kleshnin.p.o@gmail.com>
2019-01-16 20:15:40 +0300

    Updates

---
+++ precache-manifest.f2dcf5944a2d87b23fed25d33657e247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d31feaf8cfba39b7cb0",
+    "url": "/photos-app/static/js/runtime~main.2d31feaf.js"
+  },
+  {
+    "revision": "638994b6ef3b27440350",
+    "url": "/photos-app/static/js/main.638994b6.chunk.js"
+  },
+  {
+    "revision": "657a2fa461725d138381",
+    "url": "/photos-app/static/js/1.657a2fa4.chunk.js"
+  },
+  {
+    "revision": "638994b6ef3b27440350",
+    "url": "/photos-app/static/css/main.e2c863f9.chunk.css"
+  },
+  {
+    "revision": "04b4fa55fd0bec823ea6360a195801e0",
+    "url": "/photos-app/index.html"
+  }
+];
